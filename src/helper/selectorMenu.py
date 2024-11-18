adea265f66d6dc584cb5184b68379782602ce294
Georges David <georgessdavid@gmail.com>
2024-11-18 16:14:20 -0300

    Added desription to selectorMenu

--- src/helper/selectorMenu.py
+++ src/helper/selectorMenu.py
@@ -1,5 +1,14 @@
 import inquirer
+
 def selectorMenu(options: list, title: str, returnType: str):
+    '''
+    Función para crear menú de selección de opciones.
+    
+    Parámetros:
+    - options (Array): lista de opciones a seleccionar.
+    - title (String): título del menú.
+    - returnType (String): tipo de retorno de la función. Puede ser 'index', para recibir un integer con la posición de la opción, o 'value', para recibir la opción seleccionada.
+    '''
     questions = [
         inquirer.List('option', message=title, choices=options)
     ]
